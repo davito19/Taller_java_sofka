42423cc083639df3cdec4254445db6eae4f3a71c
Davito19 <omardbonett@gmail.com>
2021-11-19 12:52:16 -0500

    Update ejercicio6 taller java

---
+++ src/Ejercicio6/App.java
@@ -0,0 +1,12 @@
+package Ejercicio6;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Numeros pares del 1 al 100:");
+        for(int i = 1; i <= 50; i++) System.out.print(i*2+", ");
+        System.out.println();
+        System.out.println("Numeros impares del 1 al 100");
+        for (int i = 0; i < 50; i++) System.out.print((2*i+1)+", ");
+        System.out.println();
+    }
+}
