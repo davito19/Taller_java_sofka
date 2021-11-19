aeff2a1b45e81b92b157d0d1a2c0a720410f3f1b
Davito19 <omardbonett@gmail.com>
2021-11-19 12:23:37 -0500

    Update ejercicio 1 taller java

---
+++ src/Ejercicio1/App.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class App {
+    public static void main(String[] args) {
+        double variable1 = 2;
+        double variable2 = 3;
+
+        if(variable1 == variable2)  System.out.println("las variables son iguales");
+        else if(variable1 > variable2)  System.out.println("Variable 1 mayor a variable 2");
+        else  System.out.println("Variable 2 mayor a variable 1");
+
+    }
+}
